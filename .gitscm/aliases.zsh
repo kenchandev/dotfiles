bee3d7a5e458891e16109227ed09eb468cc755ac
Ken Chan <thiskenchan@gmail.com>
2017-08-15 00:42:53 -0400

    refactored git aliases into separate file to be sourced

---
+++ .gitscm/aliases.zsh
@@ -0,0 +1,16 @@
+alias g=git
+alias ga='git add'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gc='git commit -v'
+alias gl='git pull'
+alias gp='git push'
+alias gst='git status -sb'
+alias gsd='git svn dcommit'
+alias gsr='git svn rebase'
+alias gs='git stash'
+alias gsa='git stash apply'
+alias gr='git stash && git svn rebase && git svn dcommit && git stash pop' # git refresh
+alias gd='git diff | $GIT_EDITOR -'
+alias gmv='git mv'
+alias gho='$(git remote -v 2> /dev/null | grep github | sed -e "s/.*git\:\/\/\([a-z]\.\)*/\1/" -e "s/\.git.*//g" -e "s/.*@\(.*\)$/\1/g" | tr ":" "/" | tr -d "\011" | sed -e "s/^/open http:\/\//g" | uniq)'
