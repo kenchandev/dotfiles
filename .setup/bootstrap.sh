14019ca22cd880c424fd4d144e71ba02da35b414
Ken Chan <thiskenchan@gmail.com>
2017-08-29 00:02:29 -0400

    added status functions in bootstrap script

---
+++ .setup/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [ \033[0;31mFAIL\033[0m ] $1\n"
+  exit
+}
